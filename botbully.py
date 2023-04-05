8969b84902e357b51ba862e046f97f82dbb8cc15
nobully-osu <matthew-313@live.com>
2023-04-05 03:50:43 -0500

    Restructure and clean things up a bit.

--- botbully.py
+++ botbully.py
@@ -1,31 +1,29 @@
 from twitchio.ext import commands
+
 import super_secret
 import nowplayinglogic
 
 class Bot(commands.Bot):
 
     def __init__(self):
-        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        # prefix can be a callable, which returns a list of strings or a string...
-        # initial_channels can also be a callable which returns a list of strings...
+        #init with api key, prefix, and channels to join
         super().__init__(token=super_secret.API_KEY, prefix=super_secret.BOT_PREFIX, initial_channels=[super_secret.CHANNEL])
 
     async def event_ready(self):
-        # Notify us when everything is ready!
-        # We are logged in and ready to chat and use commands...
+        #notify when ready
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
     
     async def event_message(self, message):
-        # Messages with echo set to True are messages sent by the bot...
-        # For now we just want to ignore them...
+        #ignore messages sent by self
         if message.echo:
             return
 
+        #override event_message ignore to allow others to interact
+        await self.handle_commands(message)
+
         #print everything in chat but self sent messages (cringe) ((don't do this))
         #print(message.content)
-
-        await self.handle_commands(message)
 
     @commands.command()
     async def np(self, ctx: commands.Context):
--- nowplayinglogic.py
+++ nowplayinglogic.py
@@ -2,13 +2,6 @@ import os
 
 textfile = "C:\\Users\\Matthew\\Desktop\\Games\\osuStreamCompanion\\Files\\np_playing_DL.txt"
 
-#f = open(textfile)
-#link = f.read()
-#f.close()
-
-#with open(textfile) as f:
-#   link = f.read()
-
 def getCurrentSong():
     f = open(textfile)
     link = f.read()
